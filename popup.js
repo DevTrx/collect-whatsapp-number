2c8a08e32b8deefc73969585f5fa7e8dd7a21215
Aviran Hayun <aviran.hayun@samanage.com>
2018-04-29 16:49:52 +0300

    indentation

--- popup.js
+++ popup.js
@@ -1,62 +1,56 @@
 msDelay = 3000;
 
 document.addEventListener('DOMContentLoaded', function() {
-
   document.getElementById('colect').addEventListener('click', function() {
-      chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
-          var callFunc = function() {
-              chrome.tabs.sendMessage(activeTabs[0].id, { action: 'colect' }, function(response) {
-                  console.log('response.value', response.value);
-                  document.getElementById("textarea").value = response.value;
-                  if (response.hasMore) {
-                       setTimeout(function()
-                  {
-                          callFunc();
-
-                        }, msDelay);
-                  }
-              });
+    chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
+      var callFunc = function() {
+        chrome.tabs.sendMessage(activeTabs[0].id, { action: 'colect' }, function(response) {
+          console.log('response.value', response.value);
+          document.getElementById("textarea").value = response.value;
+          if (response.hasMore) {
+            setTimeout(function() {
+              callFunc();
+            }, msDelay);
           }
+        });
+      }
 
-          callFunc();
-
-      });
+      callFunc();
+    });
   });
 
   document.getElementById('delete_cash').addEventListener('click', function() {
-      chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
-          chrome.tabs.sendMessage(activeTabs[0].id, { action: 'delete_cash' });
-      });
+    chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
+      chrome.tabs.sendMessage(activeTabs[0].id, { action: 'delete_cash' });
+    });
   });
 
   document.getElementById('togleScroll').addEventListener('click', function() {
-      chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
-          chrome.tabs.sendMessage(activeTabs[0].id, { action: 'togleScroll' }, function(response) {
-              document.getElementById("togleScroll").innerHTML = 'togle scroll - now:' + response.value;
-          });
-
+    chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
+      chrome.tabs.sendMessage(activeTabs[0].id, { action: 'togleScroll' }, function(response) {
+        document.getElementById("togleScroll").innerHTML = 'togle scroll - now:' + response.value;
       });
+    });
   });
 
   document.getElementById('togleScrollInfinite').addEventListener('click', function() {
-      chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
-          chrome.tabs.sendMessage(activeTabs[0].id, { action: 'togleScrollInfinite' }, function(response) {
-              document.getElementById("togleScrollInfinite").innerHTML = 'togle scroll infinite - now:' + response.value;
-          });
+    chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
+      chrome.tabs.sendMessage(activeTabs[0].id, { action: 'togleScrollInfinite' }, function(response) {
+        document.getElementById("togleScrollInfinite").innerHTML = 'togle scroll infinite - now:' + response.value;
       });
+    });
   });
 
 
   document.getElementById('fixSome').addEventListener('click', function() {
-      a = {};
-      document.getElementById("textarea").value.split('\n').forEach((curr) => {
-        a[curr] = true;
-      });
-      document.getElementById("textarea").value = Object.keys(a).reduce((res, cur) => res+cur + '\n', '');
+    a = {};
+    document.getElementById("textarea").value.split('\n').forEach((curr) => {
+      a[curr] = true;
+    });
+    document.getElementById("textarea").value = Object.keys(a).reduce((res, cur) => res+cur + '\n', '');
   });
 
   document.getElementById('btnSecDelay').addEventListener('click', function() {
-      msDelay = parseInt(document.getElementById("SecDelay").value) * 1000;
+    msDelay = parseInt(document.getElementById("SecDelay").value) * 1000;
   });
-
 });
